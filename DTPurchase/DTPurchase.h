8fe5e353ef40ee654bd4eacc3d97af2054bc0a03
Dmitriy <ToropkinD@gmail.com>
2020-02-18 19:28:05 +0300

    to dynamic

---
+++ DTPurchase/DTPurchase.h
@@ -0,0 +1,19 @@
+//
+//  DTPurchase.h
+//  DTPurchase
+//
+//  Created by Дмитрий Торопкин on 18.02.2020.
+//  Copyright © 2020 Dmitriy Toropkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DTPurchase.
+FOUNDATION_EXPORT double DTPurchaseVersionNumber;
+
+//! Project version string for DTPurchase.
+FOUNDATION_EXPORT const unsigned char DTPurchaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTPurchase/PublicHeader.h>
+
+
