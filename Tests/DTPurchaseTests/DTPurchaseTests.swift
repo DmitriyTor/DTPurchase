8a2618e8135ed3c36d6476f84bb7f990864ea7f5
Emil Karimov <emvakar@gmail.com>
2020-04-23 22:59:52 +0300

    [spm] init

---
+++ Tests/DTPurchaseTests/DTPurchaseTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import DTPurchase
+
+final class DTPurchaseTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DTPurchase().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
